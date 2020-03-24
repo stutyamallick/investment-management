f37eeae89b0e9386e12ea78ecf6f16b95438a3ba
stutyamallick <stutya.mallick@gmail.com>
2020-03-24 11:11:14 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+	devServer: {
+		open: false
+	},
+	output: {
+		filename: "build.js",
+		path: path.join(__dirname, "/dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: {
+					loader: "html-loader",
+					options: { minimize: true }
+				}
+			},
+			{
+				test: /\.less$/,
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader"
+					},
+					{
+						loader: "less-loader"
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ["file-loader"]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ["file-loader"]
+			}
+		]
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: "./",
+		hot: true,
+		https: false,
+		port: 44302
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		})
+	],
+	devtool: "source-map"
+};
